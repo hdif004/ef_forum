8d5b2fac691ee2b211b22d671f4f6dc0ccf9f9f1
Hudayfa Koujdal <102149280+hdif004@users.noreply.github.com>
2025-02-18 19:23:05 +0100

    Update HomeController and index template to display recent sujets with navigation links

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Sujets;
+use App\Repository\SujetsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController{
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(SujetsRepository $sujetsRepository): Response
     {
+        $sujets = $sujetsRepository->findAll();
+
         return $this->render('home/index.html.twig', [
+            'sujets' => $sujets,
             'controller_name' => 'HomeController',
         ]);
     }
